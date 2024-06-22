1b17758333636ee3902f4ed87d03040f587dac08
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-22 18:18:04 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -50,3 +50,13 @@ function polygonStyle(feature) {
  }
 }
 
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const markers = L.geoJson(data)
+ markers.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+
